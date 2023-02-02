0ec74520a570664497f16e74e37377fe8dbf29e3
ChaseTS <3154014120@qq.com>
2023-02-02 21:01:39 +0800

    feat: 2.2

---
+++ leetcode/54.螺旋数组.js
@@ -0,0 +1,33 @@
+// 根据左右边界
+
+var spiralOrder = function(matrix) {
+    let res = []
+    let up = 0, down =( matrix.length-1), left = 0, right = (matrix[0].length-1);
+    while(true){
+        for(let i = left; i <= right; i++){
+            res.push(matrix[up][i])
+        }
+        if(++up > down){
+            break
+        }
+        for(let i = up; i <= down; i++){
+            res.push(matrix[i][right])
+        }
+        if(--right < left){
+            break
+        }
+        for(let i = right; i >= left; i--){
+            res.push(matrix[down][i])
+        }
+        if(--down < up){
+            break
+        }
+        for(let i = down; i >= up; i--){
+            res.push(matrix[i][left])
+        }
+        if(++left > right){
+            break
+        }
+    }
+    return res
+};
